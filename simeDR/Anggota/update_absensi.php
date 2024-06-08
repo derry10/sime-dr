84fbb628f6cca8080aefdb0bab76bc3410a13a23
vardrz <fatkhurrozakf@gmail.com>
2024-06-08 11:27:22 +0700

    fix: update absensi anggota by anggota_id

--- simeDR/Anggota/update_absensi.php
+++ simeDR/Anggota/update_absensi.php
@@ -19,13 +19,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Koneksi gagal: " . mysqli_connect_error());
     }
 
-    $user_id = $_SESSION['user_id'];
+    $user_id = $_SESSION['user_id']; // ini var buat apa? dibuat tapi tidak digunakan??
     $kegiatan_id = $_POST['kegiatan_id'];
+    $anggota_id = $_POST['anggota_id'];
 
     // Update tb_absensi for the given kegiatan_id
-    $update_query = "UPDATE tb_absensi SET status = 'hadir' WHERE kegiatan_id = ?";
+    $update_query = "UPDATE tb_absensi SET status = 'hadir' WHERE kegiatan_id = ? AND anggota_id = ?";
     $stmt_update = $koneksi->prepare($update_query);
-    $stmt_update->bind_param("i", $kegiatan_id);
+    $stmt_update->bind_param("ii", $kegiatan_id, $anggota_id);
 
     if ($stmt_update->execute()) {
         echo "Update berhasil!";
@@ -36,4 +37,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt_update->close();
     mysqli_close($koneksi);
 }
-?>
